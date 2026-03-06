cb4a3e84fb65c033bfd5f853d6612e4cae2af5b6
Amaury Menneret <amaury.menneret@etu.univ-lyon1.fr>
2026-03-06 10:15:40 +0100

    blue and red upgrades

--- src/main/java/com/hatedero/compendiummod/entity/BlueProjectile.java
+++ src/main/java/com/hatedero/compendiummod/entity/BlueProjectile.java
@@ -1,19 +1,23 @@
 package com.hatedero.compendiummod.entity;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.*;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -40,6 +44,33 @@ public class BlueProjectile extends AbstractHurtingProjectile {
 
         Vec3 movement = this.getDeltaMovement();
         double speedLimit = 0.5D;
+
+        if (!this.level().isClientSide()) {
+            int attempts = this.age%20*3;
+            double blockRadius = this.age%20*2;
+
+            for (int i = 0; i < attempts; i++) {
+                BlockPos randomPos = this.blockPosition().offset(
+                        this.level().random.nextInt((int) blockRadius * 2) - (int) blockRadius,
+                        this.level().random.nextInt((int) blockRadius * 2) - (int) blockRadius,
+                        this.level().random.nextInt((int) blockRadius * 2) - (int) blockRadius
+                );
+
+                BlockState state = this.level().getBlockState(randomPos);
+
+                if (!state.isAir() && state.getDestroySpeed(this.level(), randomPos) >= 0) {
+
+                    FallingBlockEntity fallingBlock = FallingBlockEntity.fall(this.level(), randomPos, state);
+
+                    fallingBlock.dropItem = false;
+
+                    this.level().removeBlock(randomPos, false);
+
+                    fallingBlock.setDeltaMovement(0, 0.1, 0);
+                    this.level().addFreshEntity(fallingBlock);
+                }
+            }
+        }
 
         if (movement.length() > speedLimit) {
             this.setDeltaMovement(movement.normalize().scale(speedLimit));
@@ -87,5 +118,39 @@ public class BlueProjectile extends AbstractHurtingProjectile {
 
     @Override
     protected void onDeflection(@Nullable Entity entity, boolean deflectedByPlayer) {
+    }
+
+    @Override
+    protected void onHitEntity(EntityHitResult result) {
+        if (!level().isClientSide()){
+            if(result.getEntity() instanceof LivingEntity entity) {
+                entity.hurt(this.level().damageSources().inWall(), 1);
+            } else {
+                result.getEntity().discard();
+            }
+        }
+        super.onHitEntity(result);
+    }
+
+    @Override
+    protected void onHitBlock(BlockHitResult result) {
+        if (!this.level().isClientSide()) {
+                BlockPos randomPos = result.getBlockPos();
+
+                BlockState state = this.level().getBlockState(randomPos);
+
+                if (!state.isAir() && state.getDestroySpeed(this.level(), randomPos) >= 0) {
+
+                    FallingBlockEntity fallingBlock = FallingBlockEntity.fall(this.level(), randomPos, state);
+
+                    fallingBlock.dropItem = false;
+
+                    this.level().removeBlock(randomPos, false);
+
+                    fallingBlock.setDeltaMovement(0, 0.1, 0);
+                    this.level().addFreshEntity(fallingBlock);
+            }
+        }
+        super.onHitBlock(result);
     }
 }
